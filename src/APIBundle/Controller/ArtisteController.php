a0ccc588c79a7acef1d601ef9dfdc84a7b8934d5
bizimunda <yourbelover@gmail.com>
2016-09-01 16:31:05 +0200

    Initial commit Main file src There we have to create the Bundle Bundle contain everything

---
+++ src/APIBundle/Controller/ArtisteController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: temp
+ * Date: 1/09/2016
+ * Time: 16:06
+ */
+namespace APIBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ArtisteController extends Controller  {
+
+}
