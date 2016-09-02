c63e6ea8d5e79431891e193302839b8bd4340474
bizimunda <yourbelover@gmail.com>
2016-09-02 16:44:20 +0200

    Artiste file updates Put new methods

--- src/APIBundle/Controller/ArtisteController.php
+++ src/APIBundle/Controller/ArtisteController.php
@@ -7,7 +7,75 @@
  */
 namespace APIBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArtisteController extends Controller  {
+
+    public function readAction( Request $request){
+        $get=$request->query;
+
+        if($get->has("s")){
+            return $this->forward("APIBundle:Artiste:getByName", array("request"=>$request));
+        } elseif ($get->has("as")){
+            return $this->forward("APIBundle:Artiste:getPseudoContains", array("request"=>$request));
+        }
+        die;
+
+        $em=$this->getDoctrine()->getManager();
+        $artistes=$em->getRepository("APIBundle:Artiste")->findAll();
+
+        $json=array();
+        foreach ($artistes as $artiste){
+            array_push($json,$artiste->toJSON());
+        }
+        return new JsonResponse($json);
+
+    }
+    public function createAction(){
+        echo "Create";
+    }
+
+    public function deleteAction(){
+        echo "Delete";
+    }
+
+    public function updateAction(){
+        echo "Update";
+    }
+
+    public function getPseudoContainsAction(Request $request){
+
+        $pseudoPart=$request->query->get("as");
+        $em=$this->getDoctrine()->getManager();
+        $artistes=$em->getRepository("APIBundle:Artiste")->findByPseudoPart(array(
+            "pseudo"=>$pseudoPart
+        ));
+        $json=array();
+        foreach ($artistes as $artiste){
+            array_push($json,$artiste->toJSON());
+        }
+
+
+        return new JsonResponse($json);
+
+    }
+
+    public function getByNameAction(Request $request){
+
+        $pseudo=$request->query->get("s");
+        $em=$this->getDoctrine()->getManager();
+        $artistes=$em->getRepository("APIBundle:Artiste")->findBy(array(
+           "pseudo"=>$pseudo
+        ));
+        $json=array();
+        foreach ($artistes as $artiste){
+            array_push($json,$artiste->toJSON());
+        }
+
+
+        return new JsonResponse($json);
+
+    }
 
 }
---
+++ src/APIBundle/Entity/Artiste.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace APIBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Artiste
+ *
+ * @ORM\Table(name="artiste")
+ * @ORM\Entity(repositoryClass="APIBundle\Repository\ArtisteRepository")
+ */
+class Artiste
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="Id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Pseudo", type="string", length=255, nullable=true)
+     */
+    private $pseudo;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="DateDeNaissance", type="datetime", nullable=true)
+     */
+    private $dateDeNaissance;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Commentaire", type="text", nullable=true)
+     */
+    private $commentaire;
+
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Artiste
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Artiste
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set pseudo
+     *
+     * @param string $pseudo
+     *
+     * @return Artiste
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    /**
+     * Get pseudo
+     *
+     * @return string
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * Set dateDeNaissance
+     *
+     * @param \DateTime $dateDeNaissance
+     *
+     * @return Artiste
+     */
+    public function setDateDeNaissance($dateDeNaissance)
+    {
+        $this->dateDeNaissance = $dateDeNaissance;
+
+        return $this;
+    }
+
+    /**
+     * Get dateDeNaissance
+     *
+     * @return \DateTime
+     */
+    public function getDateDeNaissance()
+    {
+        return $this->dateDeNaissance;
+    }
+
+    /**
+     * Set commentaire
+     *
+     * @param string $commentaire
+     *
+     * @return Artiste
+     */
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * Get commentaire
+     *
+     * @return string
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+    public function toJSON(){
+        return array(
+          "Id"=>$this->id,
+          "Nom"=>$this->nom,
+          "Prenom"=>$this->prenom,
+          "Pseudo"=>$this->pseudo,
+          "DateDeNaissance"=>$this->dateDeNaissance,
+          "Commentaire"=>$this->commentaire
+
+        );
+    }
+}
+
